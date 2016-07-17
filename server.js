6018e7a3bc8c08f148345e805678e8d7d18e4a67
pgenssler <ulkbaaeer@freenet.de>
2016-07-17 14:38:31 +0200

    added -o parameter to pass an output path to the script

--- server.js
+++ server.js
@@ -1,16 +1,45 @@
+var optimist = require('optimist')
+		.usage('Usage: $0 -o <output file>')
+		.options('o', {
+			alias : 'output',
+			default : 'activity.txt',
+			describe : 'path to the output log file'
+		})
+		.string('o')
+		.options('h', {
+			describe : 'Display this message',
+			alias : 'help'
+		});
+
+var argv = optimist.argv;
+if (argv.help) {
+	optimist.showHelp();
+	process.exit(0);
+}
+var outputPath = argv.o;
+
+var fs = require('fs');
+fs.access(outputPath, fs.W_OK, function(err) {
+	if (err) {
+		console.error('file ' + outputPath + ' not writable.');
+		process.exit(1);
+	}
+});
+
 var express = require('express');
 var app = express();
-var fs = require('fs');
 app.set('view engine','jade');
+
+console.log('writing output to ' + outputPath);
 
 app.get('/send', function (req, res) {
     var activity = req.query['activityIn'];
     var date = new Date();
-    console.log(activity);
-    fs.appendFile('../activity.txt',date.getTime() + ' : ' + activity + '\n',  function(err) {
-   if (err) {
+    console.log(date.getTime() + ': ' + activity);
+    fs.appendFile(outputPath,date.getTime() + ' : ' + activity + '\n',  function(err) {
+    if (err) {
        return console.error(err);
-   }});
+    }});
     res.send('Done');
 });
 
@@ -19,5 +48,5 @@ app.get('/', function(req,res) {
 });
 
 app.listen(8081, function () {
-  console.log('Example app listening on port 8081!');
+  console.log('Day Tracker app listening on port 8081!');
 });
