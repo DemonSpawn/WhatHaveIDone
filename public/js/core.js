68a31bad3b5a9e0aa0f541486d64fb580e77a284
krabo0om <ulkbaaeer@freenet.de>
2016-08-02 15:43:28 +0200

    added default time for website

--- public/js/core.js
+++ public/js/core.js
@@ -1,2 +1,5 @@
-var today = new Date();
-document.getElementById('dateIn').value = today.toISOString().substring(0, 10);
+var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
+var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0,-1);
+document.getElementById('dateIn').value = localISOTime.substring(0, 10);
+document.getElementById('timeIn').value = localISOTime.substring(11, 16);
+
