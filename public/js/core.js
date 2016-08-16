2cfe0df201bb24151c11992a4ea65a99a11519a1
krabo0om <ulkbaaeer@freenet.de>
2016-08-16 11:42:24 +0200

    time/date will now update every minute so that the page does not has to be refreshed

--- public/js/core.js
+++ public/js/core.js
@@ -1,7 +1,13 @@
-var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
-var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0,-1);
-document.getElementById('dateIn').value = localISOTime.substring(0, 10);
-document.getElementById('timeIn').value = localISOTime.substring(11, 16);
+function updateClock() {
+	var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
+	var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0,-1);
+	document.getElementById('dateIn').value = localISOTime.substring(0, 10);
+	document.getElementById('timeIn').value = localISOTime.substring(11, 16);
+}
+
+// update date and time field every minute
+setInterval(updateClock, 60000);
+updateClock();		// but do it also right now
 
 $.getJSON( "last", function( data ) {
   var items = [];
